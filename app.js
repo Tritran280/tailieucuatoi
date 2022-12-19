feac8a9d9634535b57396a445f03070ea19c69c4
Tritran280 <102943113+Tritran280@users.noreply.github.com>
2022-12-19 16:03:42 +0700

    upload

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$(".file").click(function () { 
+		window.location.href = "tailieu/tailieu.html"
+	})
+
+	$(".img").click(function () { 
+		window.location.href = "tlimg/tlimg.html"
+	})
+
+	$(".link").click(function () { 
+		window.location.href = "link/link.html"
+	})
+
+	$(".code").click(function () { 
+		window.location.href = "code/code.html"
+	})
+})
